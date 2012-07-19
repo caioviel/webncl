41040ca81d4381c1436afb8d07d6203403ce80e1
alexfqc <alessander70@gmail.com>
2012-07-19 13:39:11 -0300

    attrClip implementado

--- luaCanvas.js
+++ luaCanvas.js
@@ -1,9 +1,21 @@
 //ok
+var x = 0;
+var y = 0;
+var tamanhoX = 0;
+var tamanhoY = 0;
+var inicioX = 0;
+var inicioY = 0;
+var fimX = 0;
+var fimY = 0;
+
+
+
 function createContext(id, w, h) {
     var canvas = document.createElement('canvas');
     canvas.id = id;
     canvas.width = w;
     canvas.height = h;
+    canvas.setAttribute('style', 'border: black 1px solid');
     ctx = canvas.getContext("2d");
     
    
@@ -13,6 +25,24 @@ function createContext(id, w, h) {
 	return ctx;  
 	       
 }
+
+
+
+
+function attrClip(xx, yy, w, h, ctx){
+	
+	x = xx;
+	y= yy;
+	tamanhoX = w;
+	tamanhoY = h; 
+	
+	
+	ctx.lineWidth = "1";
+	ctx.strokeRect(x,y,tamanhoX,tamanhoY);
+   
+	
+}    
+
 
 //ok
 function attrSize(id){
@@ -42,33 +72,106 @@ function attrColor(ctx, r, g, b, a, modo){
 		
 }
 
+//verifica se o tamanho solicitado eh superior ao permitido no clip
+function IniVerifClip(x1, y1, x2, y2){	
+	
+	inicioX = x + x1;
+	inicioY = y + y1;
+	fimX = x + x2;
+	fimY = y + y2;
+	var verifica = true;
+	
+	if(fimX > tamanhoX)
+		fimX = tamanhoX+x;
+		
+	if(fimY > tamanhoY)
+		fimY = tamanhoY+y;
+		
+	if(inicioX > tamanhoX-2 || inicioY > tamanhoY-2){
+		verifica = false;
+		
+	}
+		
+	return verifica;	
+}
+
+function IniVerifClip2(x1, y1, x2, y2){	
+	
+	inicioX = x + x1;
+	inicioY = y + y1;
+	fimX = x + x2;
+	fimY = y + y2;
+	var verifica = true;
+	
+	if(inicioX > tamanhoX-2 || inicioY > tamanhoY-2){
+		verifica = false;
+		
+	}
+		
+		
+	
+	if(fimX > tamanhoX)
+		fimX = tamanhoX+x-inicioX;
+		
+	if(fimY > tamanhoY)
+		fimY = tamanhoY+y-inicioY;
+		
+	return verifica;
+			
+}
+
 
 //ok		
 function drawLine(ctx, x1, y1, x2, y2){
 	
-	ctx.beginPath();
-	ctx.moveTo(x1,y1);
-	ctx.lineTo(x2,y2);
-					
-	ctx.stroke(); 
+	
+	
+	var verifica = IniVerifClip(x1, y1, x2, y2);
+	
+	if(verifica){
+		ctx.beginPath();
+		ctx.moveTo(inicioX,inicioY);
+		ctx.lineTo(fimX,fimY);
+						
+		ctx.stroke(); 
+	}
+	
+	else{
+		alert("Coordenadas fora da area selecionada para desenho")
+	}
+	
+	
 		
 }
 
 //ok
 function drawRect(ctx, x1, y1, x2, y2, modo){
 	
-	if(modo == "fill"){
-		ctx.fillRect(x1, y1, x2, y2);
-	}
+	var verifica = IniVerifClip2(x1, y1, x2, y2);
 	
-	else if(modo == "frame"){
-		ctx.strokeRect(x1,y1,x2,y2);
+	
+	
+	if(verifica){
+		if(modo == "fill"){
+			ctx.fillRect(inicioX, inicioY, fimX, fimY);
+		}
+		
+		else if(modo == "frame"){
+			ctx.strokeRect(inicioX, inicioY, fimX, fimY);
+		}
+		
+		else{
+			alert("modo não existe - Escolha fill ou frame");
+			
+		}
 	}
 	
 	else{
-		alert("modo não existe - Escolha fill ou frame");
-		
+		alert("Coordenadas fora da area selecionada para desenho")
 	}
+	
+	
+	
 		
 		
 }
@@ -115,20 +218,13 @@ function image_path(caminho){
 	
 }
 
-function attrClip(ctx,id, w, h) {
-    var canvas = document.createElement('canvas');
-    canvas.id = id;
-    canvas.width = w;
-    canvas.height = h;
-    ctx2 = canvas.getContext("2d");
-    
-   
-  	    
-    document.body.appendChild(ctx);
-	    
-	return ctx;  
-	       
+function flush(ctx){
+	
+	ctx.save();
+	
 }
+
+
 
 
   
