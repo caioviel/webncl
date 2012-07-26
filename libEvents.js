897d4052d925599ebe3b725def532913a56ffc33
alexfqc <alessander70@gmail.com>
2012-07-26 14:27:04 -0300

    functions timer() and post() created

--- libEvents.js
+++ libEvents.js
@@ -5,15 +5,29 @@ function libEvents(){
 };
 
 
-libEvents.prototype.register = function(secletor, evt, fct){
+libEvents.prototype.register = function(selcetor, evt, fct){
 	
 	$(selector).bind(evt,fct);
 	
 }
 
-libEvents.prototype.unregister = function(secletor, evt, fct){
+libEvents.prototype.unregister = function(selcetor, evt, fct){
 	
 	$(selector).unbind(evt,fct);
+}
+
+libEvents.prototype.post = function(evt){
+	
+	//importing from webncl.js
+	player.postEvent(evt);	
+	
+}
+
+libEvents.prototype.timer = function(t,callback,manager){
+	
+	//importing from Timer.js
+	manager.add(t,callback);	
+	
 }
 
 
